a096fe48f8c07476633495216146d0d2e6bf76bb
riley <rzfvansuijdam@gmail.com>
2021-02-08 12:30:53 +0100

    added 1st assignment

---
+++ Assets/Coroutines.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coroutines : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(Coroutine());
+    }
+
+    public IEnumerator Coroutine()
+    {
+        print("Ik start nu de coroutine ");
+        yield return new WaitForSeconds(.5f);
+        print("coroutine update ");
+        yield return new WaitForSeconds(.5f);
+        print("coroutine einde ");
+        yield return null;
+
+    }
+}
